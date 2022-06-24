8ee206db7f841bc5b802c5927b828178480fe9c7
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-06-24 16:38:28 +0100

    list validations done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  has_many :tasks
+
+  validates :title, presence: true, length: { maximum: 25 }, allow_blank: false
+  validades :description, length: { maximum: 150 }, allow_blank: true
 end
