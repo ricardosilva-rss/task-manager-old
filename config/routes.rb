bea8cfda168ce41252541405a16e61d62c0d37c4
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-06-25 23:22:33 +0100

    took task#index route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists do
-    resources :tasks
+    resources :tasks, except: [:index]
   end
 end
