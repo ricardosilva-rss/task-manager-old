e5723fba3b386ab927770ebd2b2bcecf1395d52e
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-06-29 17:25:51 +0100

    added links in index to each list show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,14 @@
 class ListsController < ApplicationController
+  before_action :set_params, only: :show
   def index
     @lists = List.all
+  end
+
+  def show; end
+
+  private
+
+  def set_params
+    @list = List.find(params[:id])
   end
 end
