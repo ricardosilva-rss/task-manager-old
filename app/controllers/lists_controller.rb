03ef195328a8be187db41d106bdfd7da829a8d2a
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-06-30 20:19:26 +0100

    lists#new and #create are working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,32 @@
 class ListsController < ApplicationController
-  before_action :set_params, only: :show
+  before_action :set_list, only: :show
   def index
     @lists = List.all
   end
 
   def show; end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save!
+      redirect_to list_path(@list), notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
-  def set_params
+  def set_list
     @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:title, :description)
   end
 end
