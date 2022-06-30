221e6bf6593c60e27010a2798f7f1898f6474cf4
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-06-30 20:35:15 +0100

    lists#edit and #update are working

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: :show
+  before_action :set_list, only: [:show, :edit, :update]
   def index
     @lists = List.all
   end
@@ -17,6 +17,16 @@ class ListsController < ApplicationController
       redirect_to list_path(@list), notice: 'List was successfully created.'
     else
       render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @list.update(list_params)
+      redirect_to list_path(@list), notice: 'List was successfully updated.'
+    else
+      render :edit
     end
   end
 
