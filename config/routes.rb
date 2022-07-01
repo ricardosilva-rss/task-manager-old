07b4deba5cfc3adcce976a7a4f30d99b0cc91443
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-07-01 10:42:29 +0100

    task controller and its views generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks/show'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
   devise_for :users
   root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
