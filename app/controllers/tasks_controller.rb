f321564962885840d4438183b7f444c8ea4d779c
Ricardo Silva <ricardosilva.rss@gmail.com>
2022-07-05 23:55:30 +0100

    tasks#new#create are working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,23 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :new]
 
   def show; end
 
   def new
+    @task = Task.new
+    @task.list_id = @list.id
   end
 
   def create
+    @list = List.find(params[:list_id])
+    @task = Task.new(task_params)
+    @task = @list.save
+
+    if @task.save
+      redirect_to list_path(@list), notice: 'List was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -24,7 +35,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:list_id])
   end
 
-  def set_params
+  def task_params
     params.require(:task).permit(:title, :description, :concluded, :start_date, :end_date, :address)
   end
 end
